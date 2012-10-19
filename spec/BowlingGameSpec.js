b73da82380d83cb319411093f32405d47a44430b
juanignaciosl <juanignaciosl@gmail.com>
2012-10-19 05:03:55 -0400

    First solution

---
+++ spec/BowlingGameSpec.js
@@ -0,0 +1,80 @@
+/**
+ * My first kata ever :_) http://www.solveet.com/exercises/Kata-Bowling/10 .
+ * I wanted to try Jasmine as well as trying some best practices of 'Javascript: The Good Parts'.
+ */
+describe("BowlingGame", function() {
+    var game;
+    
+    it("should score 10 if the player rolls 1 strike", function() {
+        game = bowlingGame("X");
+        expect(game.score()).toBe(10);
+    });
+    
+    it("should score 30 if the player rolls 2 strikes", function() {
+        game = bowlingGame("XX");
+        expect(game.score()).toBe(30);
+    });
+    
+    it("should score 60 if the player rolls 3 strikes", function() {
+        game = bowlingGame("XXX");
+        expect(game.score()).toBe(60);
+    });
+    
+    it("should score 90 if the player rolls 4 strikes", function() {
+        game = bowlingGame("XXXX");
+        expect(game.score()).toBe(90);
+    });
+    
+    it("should score 240 if the player rolls 9 strikes", function() {
+        game = bowlingGame("XXXXXXXXX");
+        expect(game.score()).toBe(240);
+    });
+    
+    it("should score 270 if the player rolls 10 strikes", function() {
+        game = bowlingGame("XXXXXXXXXX");
+        expect(game.score()).toBe(270);
+    });
+    
+    it("should score 300 if the player scores 12 strikes", function() {
+        game = bowlingGame("XXXXXXXXXXXX");
+        expect(game.score()).toBe(300);
+    });
+    
+    it("should score 9 if the player rolls 9 and a miss", function() {
+        game = bowlingGame("9-");
+        expect(game.score()).toBe(9);
+    });
+    
+    it("should score 90 if the player rolls 10 pairs of 9 and miss", function() {
+        game = bowlingGame("9-9-9-9-9-9-9-9-9-9-");
+        expect(game.score()).toBe(90);
+    });
+    
+    it("should score 10 if the player rolls 1 pair of 5 and spare", function() {
+        game = bowlingGame("5/");
+        expect(game.score()).toBe(10);
+    });
+    
+    it("should score 25 if the player rolls 2 pairs of 5 and spare", function() {
+        game = bowlingGame("5/5/");
+        expect(game.score()).toBe(25);
+    });
+    
+    it("should score 150 if the player rolls 10 pairs of 5 and spare, with a final 5", function() {
+        game = bowlingGame("5/5/5/5/5/5/5/5/5/5/5");
+        expect(game.score()).toBe(150);
+    });
+    
+    it("should have 10 frames if the player rolls 10 pairs of 9 and miss", function() {
+        game = bowlingGame("9-9-9-9-9-9-9-9-9-9-");
+        expect(game.frames.length).toBe(10);
+    });
+});
+
+describe("nullFrame", function() {
+    
+    it("should not be strike", function() {
+        expect(nullFrame().isStrike()).toBe(false);
+    });
+    
+});
---
+++ src/BowlingGame.js
@@ -0,0 +1,109 @@
+var bowlingGame = function(rolls) {
+
+    var that = {};
+    
+    var generateFrames = function(rolls) {
+        var frames = [];
+        var f = 0;
+        for(var r = 0, maxR = rolls.length; r < maxR; r++) {                        
+            frames[f] = frame(rolls[r]);
+            if(!frames[f].isStrike()) {
+                frames[f] = frame(rolls[r]+rolls[r+1]);
+                r++;
+            }           
+            f++;
+        }
+        return frames;
+    }    
+    
+    var frames = generateFrames(rolls);
+    
+    that.frames = frames;
+    
+    var bonusFrame = function(frameNumber) {
+        return frameNumber >= 10;
+    }
+    
+    var finalFrame = function(frameNumber) {
+        return frameNumber == 9;
+    }
+    
+    var score = function() {
+        var score = 0;
+        
+        for(var f = 0, maxF = frames.length; f < maxF; f++) {
+            var frame = frames[f];            
+            var knockedDownPins = frame.knockedDownPins();   
+            
+            var rollScore = knockedDownPins;                                    
+            
+            if(!finalFrame(f) && !bonusFrame(f)) {
+                if(frame.isStrike() || frame.isSpare()) {
+                    var nextFrame = f < maxF-1 ? frames[f+1] : nullFrame();
+                    rollScore += nextFrame.firstRollScore();
+                }                
+                if(frame.isStrike()) {
+                    var twoFramesAfter = f < maxF-2 ? frames[f+2] : nullFrame();
+                    rollScore += twoFramesAfter.firstRollScore();
+                }
+            }
+            score += rollScore;
+        }
+        return score;
+    }
+    that.score = score;
+    
+    return that;
+};
+
+var frame = function(value) {    
+    var STRIKE = "X";
+    var SPARE = "/";
+    var MISS = "-";
+    
+    var that = {};    
+        
+    var knockedDownPins = function() {
+        var down = 0;
+        if(value === STRIKE) {
+             down = 10;
+        } else if(value === MISS) {
+             down = 0;
+        } else if(value.indexOf(SPARE) != -1) {
+             down = 10;
+        } else {
+            down = parseInt(value);
+        }       
+        return down;
+    }
+    that.knockedDownPins = knockedDownPins;
+    
+    var firstRollScore = function() {
+        var down = 0;
+        if(value.indexOf(SPARE) != -1) {
+             down = parseInt(value[0]);
+        } else {
+            down = knockedDownPins();
+        }       
+        return down;
+    }
+    that.firstRollScore = firstRollScore;
+    
+    var isStrike = function() {
+        return value === STRIKE;
+    }
+    that.isStrike = isStrike;
+    
+    var isSpare = function() {
+        return value.indexOf(SPARE) != -1;
+    }
+    that.isSpare = isSpare;
+        
+    return that;
+};
+
+var nullFrame = function() {
+    var NULL_ROLL = "0";
+    var that = frame(NULL_ROLL);
+    return that;
+};
